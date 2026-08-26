22d255ff0ce033164ce1f891af9b42e0ed094b7d
Bruno Bastos <b.bastosds@proton.me>
2026-08-25 22:09:53 -0300

    Curso de PHP

---
+++ login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>App Help Desk</title>
+</head>
+<body>
+    <main>
+        <form action="valida.php" method="POST">
+            <div>
+                <h1>Help Desk</h1>
+            </div>
+            <div>
+                <label for="email">E-mail</label>
+                <input type="email" name="email" id="email" required>
+            </div>
+            <div>
+                <label for="senha">Senha</label>
+                <input type="password" name="senha" id="senha" required>
+            </div>
+            <div>
+                <input type="submit" value="LOGIN" class="btn-login">
+            </div>
+        </form>
+    </main>
+</body>
+</html>
---
+++ valida.php
@@ -0,0 +1,13 @@
+<?php
+
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+    echo "Bem-vindo, ";
+
+    $usuarios = array(
+        array('email' => "bastos@teste.com.br", 'senha' => "123"),
+        array('email' => "musa@teste.com.br", 'senha' => "456"),
+    );
+
+?>
+
