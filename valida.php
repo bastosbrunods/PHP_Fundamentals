0aebd338330870e6c4dc38a1b28d5cb851c0849d
Bruno Bastos <b.bastosds@proton.me>
2026-08-26 21:40:24 -0300

    Atualização no login e erro do app help desk

--- valida.php
+++ valida.php
@@ -1,13 +1,28 @@
 <?php
 
-    $email = $_POST['email'];
-    $senha = $_POST['senha'];
-    echo "Bem-vindo, ";
-
     $usuarios = array(
         array('email' => "bastos@teste.com.br", 'senha' => "123"),
         array('email' => "musa@teste.com.br", 'senha' => "456"),
     );
+
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+    $loginValido = false;
+
+    foreach($usuarios as $user){
+
+        if($user['email'] == $email && $user['senha'] == $senha){
+            $loginValido = true;
+            break;
+        }
+    }
+
+    if($loginValido){
+        echo 'Login realizado com sucesso!';
+    } else{
+        header('Location: login.php?login=erro');
+        exit;
+    }
 
 ?>
 
